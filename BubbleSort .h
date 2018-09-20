83d2dc7488860ffc9fe6d0388e4164b45689c3f3
Aykull <31603222+Aykull@users.noreply.github.com>
2018-09-20 01:29:41 -0600

    Add files via upload
    BBS base, falta adaptar

---
+++ BubbleSort .h
@@ -0,0 +1,41 @@
+//
+// Created by aykull on 18/09/18.
+//
+#include <stdio.h>
+#include "MPointer.cpp"
+
+/* Base del BBS, falta adaptar
+void swap(int *xp, int *yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// implementacion del BBS
+void bubbleSort(int arr[], int n){
+    int i, j;
+    for (i = 0; i < n-1; i++)
+
+        // Last i elements are already in place
+        for (j = 0; j < n-i-1; j++)
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+}
+
+//imprime un array
+void printArrays(int arr[], int size){
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    //printf("n");
+}
+
+// test del BBS
+int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+
+*/
