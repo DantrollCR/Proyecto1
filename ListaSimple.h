8ebe74d04f8a23d7d0e3263502ccfefd5aba5fd0
luisk97 <30986170+luisk97@users.noreply.github.com>
2018-09-28 01:34:36 -0600

    Se implemento el MPointerGC

--- ListaSimple.h
+++ ListaSimple.h
@@ -2,14 +2,17 @@
 // Created by aykull on 20/09/18.
 //
 
-
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
 
 
 struct Node{
-    int data;
+    int* data;
+    int referencias = 0;
+    int id;
+    int idMax;
+    int size;
     struct Node *next;
 };
 
@@ -23,28 +26,57 @@ void add(SimpleList &list, int data){
     list  = list1;
 }
 */
-//agrega al final
-void add(SimpleList &list, int data){
+
+
+template<typename T>
+int add(SimpleList &list, T* data){
+    int* dato = (int*)data;
     SimpleList l1, l2 = new(struct Node);
 
-    l2->data  = data;
+    l2->data  = dato;
     l2->next = NULL;
 
     if(list==NULL){
         list = l2;
+        list->id = 1;
+        list->referencias=1;
+        list->idMax = 1;
+        list->size++;
+        return 1;
     }
     else{
+        int indice = 2;
         l1 = list;
         while(l1->next!=NULL){
-            l1 = l1->next;
+            if(l1->data == dato){
+                //cout<<"Si entramos"<<endl;
+                l1->referencias++;
+                return l1->id;
+            }else {
+                l1 = l1->next;
+                indice++;
+            }
         }
-        l1->next = l2;
-    }
+        if(l1->data == dato) {
+            //cout<<"Si entramos3"<<endl;
+            l1->referencias++;
+            return l1->id;
+        }else{
+            //cout<<"Si entramos4"<<endl;
+            l2->referencias++;
+            l1->next = l2;
+            list->idMax++;
+            l2->id = list->idMax;
+            list->size++;
+            return l2->id;
+        }
 
+    }
 }
 
-
-void search(SimpleList list, int data) {
+template<typename T>
+void search(SimpleList list, T* data) {
+    int* dato = (int*) data;
     SimpleList l1 = list;
     int i = 1, band = 0;
 
@@ -61,40 +93,59 @@ void search(SimpleList list, int data) {
         cout<<"\n\n Numero no encontrado..!"<< endl;
 }
 
-void print(SimpleList list) {
+template<typename T>
+int mostrarLista(SimpleList &list, T* data) {
     int i = 0;
-
-    while(list != NULL) {
-        cout <<' '<< i+1 <<") " << list->data << endl;
-        list = list->next;
-        i++;
+    if(list == NULL) {
+        cout << "Lista Vacia!!"<<endl;
+    }else{
+        while (list != NULL) {
+            cout << ' ' << i + 1 << ") " << "id:" << list->id << " , direccion guarda:" << list->data
+                 << " , referencias:" << list->referencias << endl;
+            list = list->next;
+            i++;
+        }
     }
+    return 1;
 }
 
-
-void deleteSL(SimpleList &list, int data) {
+template <typename T>
+void deleteSL(SimpleList &list, T* data) {
+    int* dato = (int*) data;
     SimpleList l1,l2;
     l1 = list;
 
     if(list!=NULL) {
-
         while(l1!=NULL) {
+            if(l1->data==dato) {
+                if (l1->referencias == 1) {
+                    //cout<<"OKY"<<endl;
+                    if (l1->data != NULL){
+                        //cout<<"Efectivamente no era NULL y lo estamos liberando"<<endl;
+                        free(l1->data);
+                        l1->data = NULL;
+                    }
+                    if (l1 == list) {
+                        (list->next)->size = (list->size)-1;
+                        (list->next)->idMax = (list->idMax);
+                        list = list->next;
+                    }else{
+                        l2->next = l1->next;
+                    }
+                    return;
 
-            if(l1->data==data) {
+                }else{
+                    //cout<<"OKYDOKY"<<endl;
+                    l1->referencias--;
+                    return;
+                }
 
-                if(l1==list)
-                    list = list ->next;
-                else
-                    l2->next = l1->next;
-
-                delete(l1);
-                return;
             }
             l2 = l1;
             l1 = l1->next;
         }
+        //cout<<"El dato que se desea eliminar no existe"<<endl;
     }
     else
         cout<<" Lista vacia..!";
 }
-
