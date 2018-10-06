70672fab2c6b277a6bf9138cff8ff653d200754f
Aykull <31603222+Aykull@users.noreply.github.com>
2018-10-05 21:28:03 -0600

    Update QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -3,64 +3,49 @@
 //
 
 #include<stdio.h>
-#include "MPointer.cpp"
-//falta adaptar
 
-/*Base sin adaptar al Mpointer del QS
- * void swapQS(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
 
-int partition (int arr[], int low, int high) {
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // indice del elemento mas peque;o
+int partition(Lista lista,int low, int high) {
+    int pivot = (&(lista.get(high))).getNum();
+    int i = (low-1);
 
-    for (int j = low; j <= high- 1; j++) {
-        // si el elemento actual es mas grande o mas peque;o que el pivot
-        if (arr[j] <= pivot) {
-            i++;    // incrementa indice que menor elemento
-            swapQS(&arr[i], &arr[j]);
+    for (int j = low; j <= high - 1; j++) {
+        if ((&(lista.get(j))).getNum() <= pivot) {
+            i++;
+            Nodo aux;
+            aux =  (&(lista.get(i)));
+            lista.insertar(&(lista.get(j)),i);
+            lista.insertar(aux,j);
         }
     }
-    swapQS(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
 
-void quickSort(int arr[], int low, int high) {
+    Nodo aux;
+    aux =  (&(lista.get(i+1)));
+    lista.insertar(&(lista.get(high)),i+1);
+    lista.insertar(aux,high);
+    return (i+1);
+
+}
+void quickSort(Lista lista, int low, int high) {
+    cout << "Lista ";
+    lista.imprimirDatos();
     if (low < high) {
-    //pi es particion de indice, ahora el
-    //indice deberia estar en el lugar correcto
-
-        int pi = partition(arr, low, high);
-
-        //Separacion de elementos antes y
-        //despues de la particion
-
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pi = partition(lista, low, high);
+        quickSort(lista, low, pi - 1);
+        quickSort(lista, pi + 1, high);
     }
+
 }
 
-void printArray(int arr[], int size) {
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("n");
-}
 
-    //QS test
 
- int arr[] = {10, 7, 8, 9, 1, 5};
- int n = sizeof(arr)/sizeof(arr[0]);
- quickSort(arr, 0, n-1);
- printf("Sorted array: n");
- printArray(arr, n);
- return 0;
 
-*/
+
+
+
+
+
+
 
 
 
